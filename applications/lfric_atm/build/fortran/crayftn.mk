6c5415065a9865e8e1c5268253c777c76b651bcd
jamescunningham-smith <enquiries@metoffice.gov.uk>
2024-09-03 16:57:38 +0000

    ticket:290 for Benjamin Went: Update Apps to run CCE on EXZ
    git-svn-id: https://metomi/svn/lfric_apps.xm/main/trunk@3422 3896877f-8c05-4358-bc88-7947bb4308c7

--- applications/lfric_atm/build/fortran/crayftn.mk
+++ applications/lfric_atm/build/fortran/crayftn.mk
@@ -15,3 +15,19 @@ export FFLAGS_UM_PHYSICS = -s real64
 # settings
 FFLAGS_FASTD_INIT         = $(FFLAGS_INIT) 
 FFLAGS_FASTD_RUNTIME      = $(FFLAGS_RUNTIME)
+
+#to try and ease compile time for CCE on EXZ
+ifeq ($(shell expr ${CRAYFTN_VERSION} \>= 015000000), 1)
+    %bl_imp_alg_mod_psy.o %bl_imp_alg_mod_psy.mod: private FFLAGS_SAFE_OPTIMISATION = -O0
+    %bl_imp_alg_mod_psy.o %bl_imp_alg_mod_psy.mod: private FFLAGS_DEBUG = -G0
+    %ukca_emiss_mode_mod.o %ukca_emiss_mode_mod.mod: private FFLAGS_SAFE_OPTIMISATION = -O0
+    %ukca_emiss_mode_mod.o %ukca_emiss_mode_mod.mod: private FFLAGS_DEBUG = -G0
+    %bl_exp_alg_mod_psy.o %bl_exp_alg_mod_psy.mod: private FFLAGS_SAFE_OPTIMISATION = -O0 
+    %bl_exp_alg_mod_psy.o %bl_exp_alg_mod_psy.mod: privateFFLAGS_DEBUG = -G0
+    %init_aerosol_fields_alg_mod_psy.o init_aerosol_fields_alg_mod_psy.mod: private FFLAGS_SAFE_OPTIMISATION = -O0 
+    %init_aerosol_fields_alg_mod_psy.o init_aerosol_fields_alg_mod_psy.mod: private FFLAGS_DEBUG = -G0
+    %jules_extra_kernel_mod.o %jules_extra_kernel_mod.mod: private FFLAGS_SAFE_OPTIMISATION = -O0 
+    %jules_extra_kernel_mod.o %jules_extra_kernel_mod.mod: private FFLAGS_DEBUG = -G0
+ endif
+$(info $(FFLAGS_SAFE_OPTIMISATION))
+$(info $(FFLAGS_DEBUG))
